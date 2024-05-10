911db955b936cdc5f8eb6d41679c37168e8bf77a
marsellaa <selzy14@mhs.mdp.ac.id>
2024-05-10 16:37:05 +0700

    update

--- notes/lib/widgets/note_dialog.dart
+++ notes/lib/widgets/note_dialog.dart
@@ -1,19 +1,16 @@
-import 'dart:js_util';
-
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:notes/models/note.dart';
 import 'package:notes/services/note_service.dart';
 
 class NoteDialog extends StatelessWidget {
-  final Map<String, dynamic>? note;
+  final Note? note;
   final TextEditingController _titleController = TextEditingController();
   final TextEditingController _descriptionController = TextEditingController();
 
   NoteDialog({super.key, this.note}) {
     if (note != null) {
-      _titleController.text = note!['title'];
-      _descriptionController.text = note!['description'];
+      _titleController.text = note!.title;
+      _descriptionController.text = note!.description;
     }
   }
 
@@ -25,7 +22,7 @@ class NoteDialog extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           const Text(
-            'Title',
+            'Title: ',
             textAlign: TextAlign.start,
           ),
           TextField(
@@ -34,13 +31,12 @@ class NoteDialog extends StatelessWidget {
           const Padding(
             padding: EdgeInsets.only(top: 20),
             child: Text(
-              'Description',
-              textAlign: TextAlign.start,
+              'Description: ',
             ),
           ),
           TextField(
             controller: _descriptionController,
-          )
+          ),
         ],
       ),
       actions: [
@@ -54,25 +50,25 @@ class NoteDialog extends StatelessWidget {
           ),
         ),
         ElevatedButton(
-            onPressed: () {
-              if (note == null) {
-                NoteService.addNote(
-                        _titleController.text, _descriptionController.text)
-                    .whenComplete(() {
-                  Navigator.of(context).pop();
-                });
-              } else {
-                NoteService.updateNote(
-                  note!['id'],
-                  _titleController.text,
-                  _descriptionController.text,
-                ).whenComplete(() {
-                  Navigator.of(context).pop();
-                });
-              }
-            },
-            child: Text(note == null ? 'Add' : 'Update'),
-            )
+          onPressed: () {
+            if (note == null) {
+              NoteService.addNote(Note(
+                title: _titleController.text,
+                description: _descriptionController.text,
+              )).whenComplete(() {
+                Navigator.of(context).pop();
+              });
+            } else {
+              NoteService.updateNote(Note(
+                id: note!.id,
+                title: _titleController.text,
+                description: _descriptionController.text,
+                createdAt: note!.createdAt,
+              )).whenComplete(() => Navigator.of(context).pop());
+            }
+          },
+          child: Text(note == null ? 'Add' : 'Update'),
+        ),
       ],
     );
   }
